72cffa4512fac52975062f2fb8f111ab9b7e3c78
kops <jeremie@dudie.fr>
2013-10-22 01:29:38 +0200

    Add unit test for advanced DAO

--- src/test/java/fr/itinerennes/bundler/gtfs/GtfsAdvancedDaoTest.java
+++ src/test/java/fr/itinerennes/bundler/gtfs/GtfsAdvancedDaoTest.java
@@ -6,10 +6,14 @@ import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Calendar;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.onebusaway.gtfs.model.AgencyAndId;
+import org.onebusaway.gtfs.model.Stop;
+import org.onebusaway.gtfs.model.StopTime;
 import org.onebusaway.gtfs.model.calendar.ServiceDate;
 import org.onebusaway.gtfs.services.GtfsRelationalDao;
 
@@ -50,5 +54,21 @@ public class GtfsAdvancedDaoTest {
             i++;
         }
         assertThat(i).isEqualTo(28);
+    }
+    
+    @Test
+    public void testGetStopTimesForStop() {
+        final Calendar cal = Calendar.getInstance();
+        cal.set(2012, 4, 21);
+        final Stop stop = gtfsDao.getStopForId(new AgencyAndId("1", "1001"));
+        final ServiceDate date = new ServiceDate(cal);
+        final List<StopTime> stoptimes = advancedDao.getStopTimes(stop, date);
+        assertThat(stoptimes).isNotEmpty();
+        System.out.println(stop);
+        System.out.println(stoptimes.size());
+        for (final StopTime st : stoptimes) {
+            System.out.println(st);
+        }
+        
     }
 }
