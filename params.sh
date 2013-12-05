ce96818a821a3e620be7a9da65491410a3731098
kops <jeremie@dudie.fr>
2013-12-06 00:37:27 +0100

    Externalize GTFS URL and IMAGES URL parameters

---
+++ params.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+GTFS_RELEASE=20131031
+
+IMAGES_URL="http://data.keolis-rennes.com/fileadmin/documents/Picto_lignes/Pictogrammes_100x100/Pictogrammes_100x100.zip"
+GTFS_URL="http://data.keolis-rennes.com/fileadmin/OpenDataFiles/GTFS/GTFS-${GTFS_RELEASE}.zip"
+
+export IMAGES_URL GTFS_URL
