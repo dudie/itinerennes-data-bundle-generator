a87dab5c259651f4ba14c04ea4ff29155df6028e
Jérémie <kops@users.noreply.github.com>
2014-09-28 18:23:09 +0200

    Génération pour les horaires d'hiver 2014/2015 1.1
    Version 1.1 - Septembre / Octobre

--- params.sh
+++ params.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GTFS_RELEASE=20140822
+GTFS_RELEASE=20140924
 
 IMAGES_URL="http://data.keolis-rennes.com/fileadmin/documents/Picto_lignes/Pictogrammes_100x100/Pictogrammes_100x100.zip"
 GTFS_URL="http://data.keolis-rennes.com/fileadmin/OpenDataFiles/GTFS/GTFS-${GTFS_RELEASE}.zip"
