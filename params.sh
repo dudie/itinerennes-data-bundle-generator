2d80f84cd3cdc6443ef83fd1dcd75330590e36a2
Jérémie <kops@users.noreply.github.com>
2014-12-31 13:41:55 +0100

    Horaires d'hiver 2014/2015 - Version 5.0
    Horaires d'hiver 2014/2015 - Version 5.0 - janvier/février (avec métro) (valables du 05/01/2015 au 08/02/2015)

--- params.sh
+++ params.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GTFS_RELEASE=20141212
+GTFS_RELEASE=20141230
 
 IMAGES_URL="http://data.keolis-rennes.com/fileadmin/documents/Picto_lignes/Pictogrammes_100x100/Pictogrammes_100x100.zip"
 GTFS_URL="http://data.keolis-rennes.com/fileadmin/OpenDataFiles/GTFS/GTFS-${GTFS_RELEASE}.zip"
