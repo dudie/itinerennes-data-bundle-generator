4405727db3869badbc335174e9c1dbdf5faecc23
Jérémie <kops@users.noreply.github.com>
2015-03-02 22:47:15 +0100

    Horaires d'hiver 2014/2015 - Version 7.2
    valables du 9/03/2015 au 12/04/2015

--- params.sh
+++ params.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GTFS_RELEASE=20150203
+GTFS_RELEASE=20150302
 
 IMAGES_URL="http://data.keolis-rennes.com/fileadmin/documents/Picto_lignes/Pictogrammes_100x100/Pictogrammes_100x100.zip"
 GTFS_URL="http://data.keolis-rennes.com/fileadmin/OpenDataFiles/GTFS/GTFS-${GTFS_RELEASE}.zip"
